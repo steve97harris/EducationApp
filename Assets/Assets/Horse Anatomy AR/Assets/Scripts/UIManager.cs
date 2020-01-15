6caf54003c0b4b2884d4ef669fad349d0bcc9658
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-01-15 17:32:39 +0000

    uimanager

--- Assets/Assets/Horse Anatomy AR/Assets/Scripts/UIManager.cs
+++ Assets/Assets/Horse Anatomy AR/Assets/Scripts/UIManager.cs
@@ -6,14 +6,32 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public Animator anim;
+    public int currentAnim = 0;
 
     public void Next()
     {
-        
+        if (currentAnim == 2)
+        {
+            // do nothing
+        }
+        else
+        {
+            currentAnim++;
+            anim.SetInteger("NextAnim", currentAnim);
+        }
     }
 
     public void Previous()
     {
+        if (currentAnim == 0)
+        {
+            // do nothing
+        }
+        else
+        {
+            currentAnim--;
+            anim.SetInteger("NextAnim", currentAnim);
+        }
 
     }
 }
