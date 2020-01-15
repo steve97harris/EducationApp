4ef18afae75390f36b7c5e7bbc21d9b2b86a77d5
steve97harris <34012174+steve97harris@users.noreply.github.com>
2020-01-15 16:34:42 +0000

    sdfg

---
+++ Assets/Assets/Horse Anatomy AR/Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Animator anim;
+
+    public void Next()
+    {
+        
+    }
+
+    public void Previous()
+    {
+
+    }
+}
